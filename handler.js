98b4ed76378dc992d78bd5aeb488026973bf2a73
crosshj <github@chimpjuice.com>
2021-10-11 00:52:45 -0400

    update provider

--- handler.js
+++ handler.js
@@ -1,12 +1,16 @@
 'use strict';
 
 module.exports.hello = (event, context, callback) => {
+	const body = `
+hello anonyak!
+`;
 	const response = {
 		statusCode: 200,
-		body: JSON.stringify({
-			message: 'Go Serverless v1.0! Your function executed successfully!',
-			input: event,
-		}),
+		body,
+		// body: JSON.stringify({
+		// 	message: 'Go Serverless v1.0! Your function executed successfully!',
+		// 	input: event,
+		// }),
 	};
 
 	callback(null, response);
